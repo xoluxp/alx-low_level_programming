88e01757d949e31ac5c49538de542803391fc674
xoluxp <xoluxp@gmail.com>
2023-09-20 19:19:46 -0700

    last commit 2

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,34 +1,61 @@
+#include "holberton.h"
 /**
-  * cap_string - The main  function
-  *
-  * @str: Function parameter of the string
-  *
-  * Return: Void.
-  */
-char *cap_string(char *str)
-{
-	int count = 0;
+ *_strlen - reset number
+ *Description: This function return a length for some string
+ *@s: pointer char
+ *Return: int length
+ */
 
-	while (str[count])
+int _strlen(char *s)
+{
+	int len = 0;
+
+	while (*s++)
 	{
-	while (!(str[count] >= 'a' && str[count] <= 'z'))
-		count++;
-	if (str[count - 1] == ' ' ||
-	str[count - 1] == '\t' ||
-	str[count - 1] == '\n' ||
-	str[count - 1] == ',' ||
-	str[count - 1] == ';' ||
-	str[count - 1] == '.' ||
-	str[count - 1] == '!' ||
-	str[count - 1] == '?' ||
-	str[count - 1] == '"' ||
-	str[count - 1] == '(' ||
-	str[count - 1] == ')' ||
-	str[count - 1] == '{' ||
-	str[count - 1] == '}' ||
-	count == 0)
-	str[count] -= 32;
-	count++;
+		len++;
 	}
-	return (str);
+	return (len);
+}
+/**
+ *cap_string - changes all lowercase letters
+ *@s1: pointer parameter"
+ *Description: changes all lowercase letters
+ *Return: return pointer
+ */
+char *cap_string(char *s1)
+{
+	int i, j;
+
+	for (i = 0; i < _strlen(s1) - 1; i++)
+	{
+		if (
+			s1[i] == ' ' ||
+			s1[i] == '\t' ||
+			s1[i] == '\n' ||
+			s1[i] == ',' ||
+			s1[i] == ';' ||
+			s1[i] == '.' ||
+			s1[i] == '!' ||
+			s1[i] == '?' ||
+			s1[i] == '"' ||
+			s1[i] == '(' ||
+			s1[i] == ')' ||
+			s1[i] == '{' ||
+			s1[i] == '}' ||
+			i == 0
+		)
+		{
+			for (j = 'a'; j <= 'z'; j++)
+			{
+				if (s1[i + 1] == j && i != 0)
+				{
+					s1[i + 1] = j - 32;
+				} else if (s1[i] == j && i == 0)
+				{
+					s1[i] = j - 32;
+				}
+			}
+		}
+	}
+	return (s1);
 }
