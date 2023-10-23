1abb73ce25e8400282ffaa6df37ec33821b4234c
xoluxp <xoluxp@gmail.com>
2023-10-22 18:52:14 -0700

    main updated

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,4 +1,3 @@
-#ifndef MAIN_H
 #define MAIN_H
 
 int _putchar(char c);
@@ -21,4 +20,5 @@ char *_strchr(char *s, char c);
 unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 char *_strstr(char *haystack, char *needle);
+
 #endif
