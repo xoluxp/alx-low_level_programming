e47948c5eb3eef3742418903d881ca32862061da
xoluxp <xoluxp@gmail.com>
2023-10-26 19:49:41 -0700

    edit 1 edit

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -1,8 +1,8 @@
 #include "main.h"
 
 /**
+ * print_binary - prints the binary equivalent of a decimal number
  * @n: number to print in binary
- *Betty style editting
  */
 void print_binary(unsigned long int n)
 {
